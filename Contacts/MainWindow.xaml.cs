94759e192aad3f6ed795d0b27828fd73db1520bb
Neel Aundhia <neelaundhia@gmail.com>
2020-06-29 14:59:14 +0530

    Added Item Template and Item Filtering

--- Contacts/MainWindow.xaml.cs
+++ Contacts/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Contacts.Classes;
 using SQLite;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
@@ -12,9 +13,14 @@ namespace Contacts
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Contact> contacts;
         public MainWindow()
         {
             InitializeComponent();
+
+            contacts = new List<Contact>();
+
+            ReadDatabase();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -28,7 +34,6 @@ namespace Contacts
 
         void ReadDatabase()
         {
-            List<Contact> contacts;
             using (SQLiteConnection conn = new SQLiteConnection(App.databasePath))
             {
                 conn.CreateTable<Contact>();
@@ -50,6 +55,15 @@ namespace Contacts
                 contactsListView.ItemsSource = contacts;
 
             }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox searchTextBox = sender as TextBox;
+
+            var filteredList = contacts.Where(c => c.Name.ToLower().Contains(searchTextBox.Text.ToLower())).ToList();
+
+            contactsListView.ItemsSource = filteredList;
         }
     }
 }
