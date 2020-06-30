b692bb864d392ace90bd7d537ed99effb58c576d
Neel Aundhia <neelaundhia@gmail.com>
2020-06-30 13:05:44 +0530

    Added ContactControl

---
+++ Contacts/Controls/ContactControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Contacts.Classes;
+using System.Windows.Controls;
+
+namespace Contacts.Controls
+{
+    /// <summary>
+    /// Interaction logic for ContactControl.xaml
+    /// </summary>
+    public partial class ContactControl : UserControl
+    {
+        private Contact contact;
+
+        public Contact Contact 
+        { 
+            get
+            {
+                return contact;
+            }
+            
+            set
+            {
+                contact = value;
+                nameTextBox.Text = contact.Name;
+                emailTextBox.Text = contact.Email;
+                phoneTextBox.Text = contact.Phone;
+            }
+        }
+
+        public ContactControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
